7ae76b730a79e60efac9d7a60938710692d30772
haoxiang <liaohaoxiang520@gmail.com>
2021-12-07 17:24:44 +0800

    feat: 备案链接

--- component/footer.js
+++ component/footer.js
@@ -2,7 +2,7 @@ const footer = () => {
   return (
     <div className="full-width my-0 mx-auto py-8 px-4 text-gray-600 text-center">
       <div className="mb-2">@CopyRight 2020 Neo's Blog</div>
-      <div>备案号：粤ICP备2021010447号</div>
+      <a href="https://beian.miit.gov.cn/">备案号：粤ICP备2021010447号</a>
     </div>
   )
 }
